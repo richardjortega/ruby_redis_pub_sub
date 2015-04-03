edffd6977aad2d7b62b95d3a105746303cd3e71c
Richard Ortega <richard.j.ortega2@boeing.com>
2015-04-02 19:36:02 -0700

    pub sub examples

---
+++ README.md
@@ -0,0 +1,3 @@
+#### Redis Pub/Sub Example using Ruby
+
+https://robots.thoughtbot.com/redis-pub-sub-how-does-it-work
---
+++ pub.rb
@@ -0,0 +1,16 @@
+# usage:
+# ruby pub.rb channel username
+
+require 'rubygems'
+require 'redis'
+require 'json'
+require 'debugger'
+
+$redis = Redis.new
+
+data = {user: ARGV[1]}
+
+loop do
+	msg = STDIN.gets
+	$redis.publish ARGV[0], data.merge(msg: msg.strip).to_json
+end
