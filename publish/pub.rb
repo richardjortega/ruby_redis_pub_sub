dff17d2ca3efa4c7384181139a591994e6d57c63
Richard Ortega <riortega@microsoft.com>
2017-05-03 16:52:45 -0700

    update app to use ruby 2.2.

--- publish/pub.rb
+++ publish/pub.rb
@@ -7,85 +7,15 @@ require 'json'
 require 'byebug'
 require 'awesome_print'
 
-@redis = Redis.new(host: 'redis')
+redis = Redis.new(host: 'redis')
 
-def set_devices
-  # "B016005991000520" => value
-  commands = []
-  device = {}
-  device_id = "B016005991000520"
-  device.update(device_id => { device_name: "HYBRID",
-                                components: [
-                                  {
-                                    component_id: "2",
-                                    component_type: "SeatLight#{rand(1..5)}",
-                                    data_descriptors: [
-                                      {
-                                        data_descriptor_id: "4201",
-                                        name: "Reading Light",
-                                        comments: nil,
-                                        data: "#{rand(0..1)}",
-                                        date: nil
-                                      },
-                                      {
-                                        data_descriptor_id: "4202",
-                                        name: "Call Light",
-                                        comments: nil,
-                                        data: "#{rand(0..1)}",
-                                        date: nil
-                                      }
-                                    ],
-                                    configuration_descriptors: [
-                                      {
-                                        configuration_descriptor_id: "3177",
-                                        name: "Reading Light",
-                                        comments: nil
-                                      },
-                                      {
-                                        configuration_descriptor_id: "3178",
-                                        name: "Call Light",
-                                        comments: nil
-                                      }
-                                    ]
-                                  },
-                                  {
-                                    component_id: "3",
-                                    component_type: "Motor",
-                                    data_descriptors: [
-                                      {
-                                        data_descriptor_id: "5587",
-                                        name: "Fault Status",
-                                        comments: "0 = Cleared, 1 = Active",
-                                        data: "#{rand(0..1)}",
-                                        date: nil
-                                      }
-                                    ],
-                                    configuration_descriptors: [ ]
-                                  }
-                                ]
-                              })
-
-  # Redis.new.publish channel/key, value
-  # Publish Device Message (contains all Component Messages)
-  messages = {}
-  messages[device_id] = device[device_id].to_json
-
-  device[device_id][:components].each do |component|
-    # Remove configuration descriptors
-    component = component.reject{|k,v| k == :configuration_descriptors}
-    # Publish Component Message (subset of Device Message)
-    messages["#{device_id}.c#{component[:component_id]}"] = component.to_json
-  end
-
-  @redis.pipelined do
-    messages.each do |key, value|
-      @redis.publish key, value
-      puts "Sent Message: #{key} | #{value}"
-    end
-  end
-end
+key = "irvine-ppe"
+count = 0
 
 loop do
-  set_devices
+  msg = "hello JC!"
+  puts "Sending message: #{msg}"
+  redis.publish key, msg
+  count += 1
   sleep 2
 end
--- subscribe/sub.rb
+++ subscribe/sub.rb
@@ -1,14 +1,11 @@
 require 'rubygems'
 require 'redis'
-require 'json'
 require 'byebug'
 
 @redis = Redis.new(host: 'redis', timeout: 0)
 
-@redis.subscribe("B016005991000520", "B016005991000520.c2") do |on|
+@redis.subscribe("irvine-ppe") do |on|
   on.message do |channel, msg|
-    data = JSON.parse(msg)
-    puts "Msg Received: #{channel} | #{data}"
-    puts
+    puts "Msg Received: #{channel} | #{msg}"
   end
 end
